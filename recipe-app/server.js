1168760ae8ebd5281852b340bced7a5690661b55
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-18 15:17:09 +0100

    add routing to server

--- recipe-app/server.js
+++ recipe-app/server.js
@@ -12,6 +12,12 @@ mongoose.connect(db)
     .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err));
 
+
+
+// Use routes
+const recipes = require('./routes/api/recipes');
+app.use('/api/recipes', recipes);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
