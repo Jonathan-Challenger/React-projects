7cbe6f8de95f8469848adf302dfd15f7db407230
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-18 15:10:24 +0100

    create server skeleton and connection to mongo

---
+++ recipe-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+// Connect to MongDB
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db)
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
