46bd9aa1e0532e8b4fee7dc5ab4f50a8a3a3f104
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-04 15:41:16 +0000

    test getting data with axios and basic landing with nav

--- FootballStats/client/src/App.js
+++ FootballStats/client/src/App.js
@@ -1,41 +1,69 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const onClick = async e => {
+  const homeData = async e => {
+    e.preventDefault();
+
+    const data = await axios.get("https://v3.football.api-sports.io/teams", {
+      headers: {
+        "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+      },
+      params: {
+        season: "2021",
+        league: "39",
+      },
+    });
+    console.log(data);
+  };
+
+  const standingData = async e => {
     e.preventDefault();
 
     const data = await axios.get(
-      "https://v3.football.api-sports.io/teams?league=39&season=2021",
+      "https://v3.football.api-sports.io/standings",
       {
         headers: {
           "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
         },
+        params: {
+          season: "2021",
+          league: "39",
+        },
       }
     );
+    console.log(data);
+  };
 
+  const scorerData = async e => {
+    e.preventDefault();
+
+    const data = await axios.get(
+      "https://v3.football.api-sports.io/players/topscorers",
+      {
+        headers: {
+          "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+        },
+        params: {
+          season: "2021",
+          league: "39",
+        },
+      }
+    );
     console.log(data);
   };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <button onClick={e => onClick(e)}>Test</button>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <section className='container'>
+        <h1>Football Stats App</h1>
+        <button onClick={e => homeData(e)}>Test home data</button>
+        <button onClick={e => standingData(e)}>Test standings data</button>
+        <button onClick={e => scorerData(e)}>Test scorers data</button>
+      </section>
+    </>
   );
 }
 
