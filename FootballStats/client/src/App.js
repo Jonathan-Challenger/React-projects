5977f84856c9346f92c58b47e42c76db3f749570
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-09 16:35:09 +0000

    move stuff to teamscont and then import teamscont

--- FootballStats/client/src/App.js
+++ FootballStats/client/src/App.js
@@ -1,32 +1,8 @@
 import "./App.css";
-import axios from "axios";
 import Navbar from "./components/navbar/Navbar";
-import { useEffect, useState } from "react";
-import TeamItem from "./components/teamItem/TeamItem";
+import TeamsCont from "./components/teamCont/TeamsCont";
 
-function App() {
-  const [teams, setTeams] = useState([]);
-
-  useEffect(() => {
-    homeData();
-  }, []);
-
-  const homeData = async () => {
-    const res = await axios.get("https://v3.football.api-sports.io/teams", {
-      headers: {
-        "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
-      },
-      params: {
-        season: "2021",
-        league: "39",
-      },
-    });
-
-    const teams = res.data.response;
-
-    setTeams(teams);
-  };
-
+const App = () => {
   /* const standingData = async e => {
     e.preventDefault();
 
@@ -67,16 +43,13 @@ function App() {
     <>
       <Navbar />
       <section className='container'>
-        <h1 className='heading1'>Premier League Statistics</h1>
+        <h1 className='text-center x-large'>Premier League Statistics</h1>
         <div className='container'>
-          <h2 className='heading2'>Teams</h2>
-          {teams.map(({ team }) => (
-            <TeamItem team={team} key={team.id} />
-          ))}
+          <TeamsCont />
         </div>
       </section>
     </>
   );
-}
+};
 
 export default App;
