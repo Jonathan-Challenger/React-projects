654d8957875790e6ee11bcdb862f889666536858
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-15 15:10:55 +0000

    create item for top scorer

---
+++ FootballStats/client/src/components/scorers/ScorerItem.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+
+const ScorerItem = ({ player, statistics }) => {
+  return (
+    <>
+      <div className='item-cont'>
+        <img src={player.photo} alt='Player' />
+        <p className='lead' style={{ marginBottom: "0" }}>
+          {player.name}
+        </p>
+        <div className='line'></div>
+        <div className='personal-info'>
+          <p>Age: {player.age}</p>
+          <p>From: {player.nationality}</p>
+        </div>
+        <div className='line'></div>
+        <div className='team-info'>
+          <p>Team: {statistics[0].team.name}</p>
+          <img src={statistics[0].team.logo} alt='Team Logo' />
+        </div>
+        <div className='line'></div>
+        <p className='lead' style={{ marginBottom: "0" }}>
+          Goals: {statistics[0].goals.total}
+        </p>
+      </div>
+    </>
+  );
+};
+
+ScorerItem.propTypes = {
+  player: PropTypes.object.isRequired,
+  statistics: PropTypes.object.isRequired,
+};
+
+export default ScorerItem;
