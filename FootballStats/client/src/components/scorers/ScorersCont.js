c511c1dea347ccd65776dbf60d408411fc6c64b1
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-15 15:10:43 +0000

    create container for top scorers

--- FootballStats/client/src/components/scorers/ScorersCont.js
+++ FootballStats/client/src/components/scorers/ScorersCont.js
@@ -1,5 +1,53 @@
-const StandingCont = props => {
-  return <div>Scorers</div>;
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../spinner/Spinner";
+import ScorerItem from "./ScorerItem";
+
+const ScorersCont = () => {
+  const [scorers, setScorers] = useState([]);
+
+  useEffect(() => {
+    scorersData();
+  }, []);
+
+  const scorersData = async () => {
+    const res = await axios.get(
+      "https://v3.football.api-sports.io/players/topscorers",
+      {
+        headers: {
+          "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+        },
+        params: {
+          season: "2021",
+          league: "39",
+        },
+      }
+    );
+
+    const scorers = res.data.response;
+    setScorers(scorers);
+  };
+
+  return (
+    <>
+      {scorers.length === 0 ? (
+        <Spinner />
+      ) : (
+        <>
+          <h2 className='text-center large'>Top Scorers</h2>
+          <div className='items-cont'>
+            {scorers.map(({ player, statistics }) => (
+              <ScorerItem
+                player={player}
+                statistics={statistics}
+                key={player.id}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
-export default StandingCont;
+export default ScorersCont;
