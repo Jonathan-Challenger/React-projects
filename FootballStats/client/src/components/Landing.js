f10c4ab3b7490f17dd1939c4e691ae6f1e7bf64d
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-16 15:30:27 +0000

    create landing component

---
+++ FootballStats/client/src/components/Landing.js
@@ -0,0 +1,11 @@
+const Landing = () => {
+  return (
+    <div className='container-inner'>
+      <h1 className='text-center x-large py-5 text-black'>
+        Premier League Statistics
+      </h1>
+    </div>
+  );
+};
+
+export default Landing;
