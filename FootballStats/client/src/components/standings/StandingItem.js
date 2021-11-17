d8010f3a4e5971ae2c60315821a266493705016b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-17 15:19:19 +0000

    add info to standing item

--- FootballStats/client/src/components/standings/StandingItem.js
+++ FootballStats/client/src/components/standings/StandingItem.js
@@ -1,7 +1,23 @@
 import PropTypes from "prop-types";
 
 const StandingItem = ({ item }) => {
-  return <div>{item.team.name}</div>;
+  return (
+    <div className='text-accent p-1 standing-item'>
+      <div id='team-info'>
+        <p style={{ marginRight: "0.2rem" }}>{item.rank} </p>
+        <img src={item.team.logo} alt='Team' />
+        <p style={{ marginLeft: "0.2rem" }}> {item.team.name}</p>
+      </div>
+      <p>{item.all.played}</p>
+      <p>{item.all.win}</p>
+      <p>{item.all.draw}</p>
+      <p>{item.all.lose}</p>
+      <p>{item.all.goals.for}</p>
+      <p>{item.all.goals.against}</p>
+      <p>{item.goalsDiff}</p>
+      <p>{item.points}</p>
+    </div>
+  );
 };
 
 StandingItem.propTypes = {
