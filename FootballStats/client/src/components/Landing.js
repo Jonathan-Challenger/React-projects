22663b473c4a145e4331dc70fde2980ca6583778
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-17 15:18:17 +0000

    create landing comp

--- FootballStats/client/src/components/Landing.js
+++ FootballStats/client/src/components/Landing.js
@@ -1,9 +1,12 @@
+import logo from "../img/logo.png";
+
 const Landing = () => {
   return (
-    <div className='container-inner'>
-      <h1 className='text-center x-large py-5 text-black'>
+    <div id='landing-cont'>
+      <h1 className='text-center heading py-5 text-white'>
         Premier League Statistics
       </h1>
+      <img src={logo} alt='Premier League' />
     </div>
   );
 };
