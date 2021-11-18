5b8156c0afef9d52c87bd8d2c2f68bed015dd8af
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-18 13:36:59 +0000

    move content into div

--- FootballStats/client/src/components/Landing.js
+++ FootballStats/client/src/components/Landing.js
@@ -3,10 +3,12 @@ import logo from "../img/logo.png";
 const Landing = () => {
   return (
     <div className='container-inner'>
-      <h1 className='text-center heading py-5 text-white'>
-        Premier League Statistics
-      </h1>
-      <img src={logo} alt='Premier League' />
+      <div className='landing-cont'>
+        <h1 className='text-center heading py-5 text-white'>
+          Premier League Statistics
+        </h1>
+        <img id='landing-logo' src={logo} alt='Premier League' />
+      </div>
     </div>
   );
 };
