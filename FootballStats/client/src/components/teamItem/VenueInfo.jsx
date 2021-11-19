2e5ccb9f6f56e14e5a94e0dfaeb87251a21cf09c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-19 15:06:40 +0000

    create venueinfo comp

---
+++ FootballStats/client/src/components/teamItem/VenueInfo.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+const VenueInfo = ({
+  teamInfo: {
+    venue: { name, city, capacity, address, image },
+  },
+}) => {
+  return (
+    <>
+      <div className='venue text-white'>
+        <h2 className='text-center large'>Stadium</h2>
+        <div className='basic-info2 my-1'>
+          <div className='text-info'>
+            <h2 className='lead'>{name}</h2>
+            <p>
+              Address: {address}, {city}
+            </p>
+            <p>Capacity: {capacity}</p>
+          </div>
+          <img src={image} alt='Club stadium' />
+        </div>
+      </div>
+      <div className='white-line'></div>
+    </>
+  );
+};
+
+VenueInfo.propTypes = {
+  teamInfo: PropTypes.object.isRequired,
+};
+
+export default VenueInfo;
