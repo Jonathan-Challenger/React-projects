f573829d885239c9f83558d63fc4f4b259c7cb22
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-24 14:52:17 +0000

    create notfound page

---
+++ FootballStats/client/src/components/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = props => {
+  return (
+    <section className='container-inner'>
+      <h1 className='x-large text-white'>Page Not Found</h1>
+      <p className='large text-white'>Sorry, this page does not exist</p>
+    </section>
+  );
+};
+
+export default NotFound;
