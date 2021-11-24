cc10dd6cfd76fabc42852b336b6f5f28b1bdbada
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-24 14:53:04 +0000

    add link to scorer item

--- FootballStats/client/src/components/scorers/ScorerItem.js
+++ FootballStats/client/src/components/scorers/ScorerItem.js
@@ -1,28 +1,31 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const ScorerItem = ({ player, statistics }) => {
   return (
     <>
-      <div className='item-cont'>
-        <img src={player.photo} alt='Player' />
-        <p className='lead' style={{ marginBottom: "0" }}>
-          {player.name}
-        </p>
-        <div className='line'></div>
-        <div className='personal-info'>
-          <p>Age: {player.age}</p>
-          <p>From: {player.nationality}</p>
+      <Link to={`/top-scorers/${player.id}`}>
+        <div className='item-cont text-dark'>
+          <img src={player.photo} alt='Player' />
+          <p className='lead' style={{ marginBottom: "0" }}>
+            {player.name}
+          </p>
+          <div className='line'></div>
+          <div className='personal-info'>
+            <p>Age: {player.age}</p>
+            <p>From: {player.nationality}</p>
+          </div>
+          <div className='line'></div>
+          <div className='team-info'>
+            <p>Team: {statistics[0].team.name}</p>
+            <img src={statistics[0].team.logo} alt='Team Logo' />
+          </div>
+          <div className='line'></div>
+          <p className='lead' style={{ marginBottom: "0" }}>
+            Goals: {statistics[0].goals.total}
+          </p>
         </div>
-        <div className='line'></div>
-        <div className='team-info'>
-          <p>Team: {statistics[0].team.name}</p>
-          <img src={statistics[0].team.logo} alt='Team Logo' />
-        </div>
-        <div className='line'></div>
-        <p className='lead' style={{ marginBottom: "0" }}>
-          Goals: {statistics[0].goals.total}
-        </p>
-      </div>
+      </Link>
     </>
   );
 };
