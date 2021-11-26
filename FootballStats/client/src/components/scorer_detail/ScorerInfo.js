b1dfc2c8f6d634e6ea50b7700b780afb7ce9171a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-26 15:00:29 +0000

    create scorerinfo component

---
+++ FootballStats/client/src/components/scorer_detail/ScorerInfo.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const ScorerInfo = ({
+  scorerInfo: { name, nationality, photo, weight, age, height },
+}) => {
+  return (
+    <>
+      <div className='basic-info1 my-1 text-white'>
+        <div className='text-info'>
+          <h2 className='x-large'>{name}</h2>
+          <p>Age: {age}</p>
+          <p>Nationality: {nationality}</p>
+          <p>Height: {height}</p>
+          <p>Weight: {weight}</p>
+        </div>
+        <img src={photo} alt='Player' />
+      </div>
+      <div className='white-line'></div>
+    </>
+  );
+};
+
+ScorerInfo.propTypes = {
+  scorerInfo: PropTypes.object.isRequired,
+};
+
+export default ScorerInfo;
