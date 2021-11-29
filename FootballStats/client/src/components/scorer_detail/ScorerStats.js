075c4ff4343e60a55e58b0ed8e0a99cf3392f73e
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-29 13:08:21 +0000

    add extra stats to stats component

--- FootballStats/client/src/components/scorer_detail/ScorerStats.js
+++ FootballStats/client/src/components/scorer_detail/ScorerStats.js
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 import ScorerLeague from "./ScorerLeague";
 import ScorerTeam from "./ScorerTeam";
+import ScorerExtraStats from "./ScorerExtraStats";
 
 const ScorerStats = ({
   scorerStats: { team, shots, passes, league, goals, games, dribbles },
 }) => {
   return (
     <div className='stats-info my-1 text-white'>
+      <ScorerExtraStats
+        shots={shots}
+        passes={passes}
+        goals={goals}
+        games={games}
+        dribbles={dribbles}
+      />
       <ScorerTeam team={team} />
       <ScorerLeague league={league} />
     </div>
