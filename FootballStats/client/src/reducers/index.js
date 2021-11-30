5365c9726ec53c9256735cdf94bb5be072a5969f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 12:24:11 +0000

    add teams to combinereducer

--- FootballStats/client/src/reducers/index.js
+++ FootballStats/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import scorers from "./scorers";
+import teams from "./teams";
 
 // Create rootReducer
 export default combineReducers({
   scorers,
+  teams,
 });
