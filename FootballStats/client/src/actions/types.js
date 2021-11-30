5511d349e3d8517bae578fbb4d872dcf2fbff573
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 12:24:01 +0000

    add types for teams

--- FootballStats/client/src/actions/types.js
+++ FootballStats/client/src/actions/types.js
@@ -1,2 +1,5 @@
 export const GET_SCORERS = "GET_SCORERS";
 export const GET_SCORER = "GET_SCORER";
+export const GET_TEAMS = "GET_TEAMS";
+export const GET_TEAM_STATS = "GET_TEAM_STATS";
+export const GET_TEAM_INFO = "GET_TEAM_INFO";
