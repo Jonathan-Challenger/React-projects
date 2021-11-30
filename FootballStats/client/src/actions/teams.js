6362e7cedb55062b2a8b1ca17513630c9a8a05d7
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 12:24:49 +0000

    create teams actions

---
+++ FootballStats/client/src/actions/teams.js
@@ -0,0 +1,72 @@
+import { GET_TEAMS, GET_TEAM_INFO, GET_TEAM_STATS } from "./types";
+import axios from "axios";
+
+export const getTeams = () => async dispatch => {
+  try {
+    const res = await axios.get("https://v3.football.api-sports.io/teams", {
+      headers: {
+        "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+      },
+      params: {
+        season: "2021",
+        league: "39",
+      },
+    });
+
+    const teams = await res.data.response;
+    dispatch({
+      type: GET_TEAMS,
+      payload: teams,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getTeamStats = id => async dispatch => {
+  try {
+    const res = await axios.get(
+      "https://v3.football.api-sports.io/teams/statistics",
+      {
+        headers: {
+          "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+        },
+        params: {
+          team: id,
+          league: "39",
+          season: "2021",
+        },
+      }
+    );
+
+    const teamstats = await res.data.response;
+
+    dispatch({
+      type: GET_TEAM_STATS,
+      payload: teamstats,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getTeamInfo = id => async dispatch => {
+  try {
+    const res = await axios.get("https://v3.football.api-sports.io/teams", {
+      headers: {
+        "x-apisports-key": "4b7394b710f3b701ba1b6b057b9495fd",
+      },
+      params: {
+        id: id,
+      },
+    });
+
+    const teaminfo = await res.data.response;
+    dispatch({
+      type: GET_TEAM_INFO,
+      payload: teaminfo,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
