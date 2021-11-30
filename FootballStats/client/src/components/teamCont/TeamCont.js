d62cb8fc3732483c1b6131e475d7fc289b8feb2b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 12:22:59 +0000

    change name of comp

---
+++ FootballStats/client/src/components/teamCont/TeamCont.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import TeamItem from "../teamItem/TeamItem";
+import Spinner from "../spinner/Spinner";
+import { connect } from "react-redux";
+import { getTeams } from "../../actions/teams";
+import PropTypes from "prop-types";
+
+const TeamCont = ({ getTeams, teams: { teams, loading } }) => {
+  useEffect(() => {
+    getTeams();
+  }, [getTeams]);
+
+  return (
+    <div className='container-inner'>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h2 className='text-center large text-accent'>Teams</h2>
+          <div className='items-cont'>
+            {teams.map(({ team }) => (
+              <TeamItem team={team} key={team.id} />
+            ))}
+            test
+          </div>{" "}
+        </>
+      )}
+    </div>
+  );
+};
+
+TeamCont.propTypes = {
+  getTeams: PropTypes.func.isRequired,
+  teams: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  teams: state.teams,
+});
+
+export default connect(mapStateToProps, { getTeams })(TeamCont);
