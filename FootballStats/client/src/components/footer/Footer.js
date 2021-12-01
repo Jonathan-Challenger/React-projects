f1c8d59636dd6412ea298ad8b7b2f41678b11c0a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-01 15:45:53 +0000

    add noreferrer

--- FootballStats/client/src/components/footer/Footer.js
+++ FootballStats/client/src/components/footer/Footer.js
@@ -4,16 +4,25 @@ const Footer = () => {
   return (
     <footer>
       <div className='socials'>
-        <a href='https://github.com/Jonathan-Challenger' target='_blank'>
+        <a
+          href='https://github.com/Jonathan-Challenger'
+          target='_blank'
+          rel='noreferrer'
+        >
           <i className='fab fa-github fa-2x'></i>
         </a>
         <a
           href='https://www.linkedin.com/in/jonathan-challenger-448a701b3/'
           target='_blank'
+          rel='noreferrer'
         >
           <i class='fab fa-linkedin fa-2x'></i>
         </a>
-        <a href='https://www.jonathan-challenger.com/' target='_blank'>
+        <a
+          href='https://www.jonathan-challenger.com/'
+          target='_blank'
+          rel='noreferrer'
+        >
           <i class='fas fa-globe fa-2x'></i>
         </a>
       </div>
